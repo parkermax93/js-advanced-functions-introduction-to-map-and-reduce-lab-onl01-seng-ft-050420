b953b10b2ebaf5840cb334b12753d57ae18dc950
Parker Williamson <parker.williamson@gmail.com>
2020-08-21 03:34:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Your code here
+// Add your functions here
+const map = (arr, callback) => {
+    let newArr = [];
+    arr.forEach( e => newArr.push(callback(e)));
+    return newArr;
+}
+
+const reduce = (array, reducer, initialValue = array[0]) => {
+    let value = initialValue;
+
+    if (value != array[0]) {
+    array.forEach( e => {
+      value = reducer(value, e)
+    })
+    return value;
+  } else {
+    for (let i = 1; i < array.length; i++) {
+        let e = array[i]
+        value = reducer(value, e)
+    }
+  }
+  return value;
+};
